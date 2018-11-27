9bb189ad5c5a75be3b8d10453d92dfb263848bde
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-27 17:25:36 +0100

    switch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String eingabe = JOptionPane.showInputDialog("Geben Sie etwas ein:");
+
+
+        switch(eingabe){
+            case "20":
+                int zahl = Integer.parseInt(eingabe);
+                int alter = zahl * 18;
+                System.out.println(alter);
+                break;
+
+            case "Peterpan":
+                System.out.println("Bangarang");
+                break;
+        }
     }
 }
