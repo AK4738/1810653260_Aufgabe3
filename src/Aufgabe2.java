0b6e69ded0b1dce75c786ebb709419476935eb7d
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-27 18:07:09 +0100

    Augabe3

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -1,14 +1,23 @@
 public class Aufgabe2 {
     public static void main(String[] args){
-        printName();
+
         for (int i=0; i<=2; i+=2){// i+=2 inkrementiert i um 2 und somit wird Kadir nur 2mal angezeigt
-            System.out.println(printName());
+            printName();
         }
+        for (int f=0; f<=4; f++) {
+                printName();
+                printAge(f+20);
+            }
+            printAge(20);
+        }
+
+
+    public static void printName(){
+        String a = "Kadir";
+        System.out.println(a);
     }
 
-    public static String printName(){
-        String a = "Kadir";
-        return a;
-
+    public static void printAge(int age){
+        System.out.println(age);
     }
 }
