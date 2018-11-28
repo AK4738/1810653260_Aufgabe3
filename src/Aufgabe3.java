10d1fea123265f4303b43b1c89ab0ae165a75f63
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-28 12:05:42 +0100

    aufgabe 3

--- src/Aufgabe3.java
+++ src/Aufgabe3.java
@@ -1,5 +1,12 @@
 public class Aufgabe3 {
     public static void main (String [] args){
 
+        String name;
+        int alter;
+        printPerson(18,"Kadir");
+    }
+
+    public static void printPerson(int alter, String name){
+        System.out.println(alter + "  " + name);
     }
 }
