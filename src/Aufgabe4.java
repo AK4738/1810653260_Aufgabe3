7d641a09dfe8daafeaab3411f4ea3da0ba80d575
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-28 12:24:55 +0100

    aufgabe 4

---
+++ src/Aufgabe4.java
@@ -0,0 +1,14 @@
+public class Aufgabe4 {
+    public static void main(String[] args){
+        System.out.println(add(5,6) + "\n" + add(5.5, 6.4));
+    }
+
+    public static int add(int a, int b) {
+
+        return a+b;
+    }
+
+    public static double add(double a, double b){
+        return a+b;
+    }
+}
